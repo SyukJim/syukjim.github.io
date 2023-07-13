f67c9b3f3353a05737fc49a6dd4c15c79adfc3cd
Syukri <msyukri.hjahmad@gmail.com>
2023-07-13 09:41:22 +0800

    added click to enlarge image for timeline website

--- pixelated_timeline/script/script.js
+++ pixelated_timeline/script/script.js
@@ -1,3 +1,15 @@
 function enlargeImage(image){
     image.classList.toggle("enlarged");
 }
+
+function showImage(imageSrc) {
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("modalImage");
+    modal.style.display = "block";
+    modalImg.src = imageSrc;
+}
+
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
